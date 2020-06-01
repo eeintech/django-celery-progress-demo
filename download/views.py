eb2e901650bf3f80db03952ad9dcdccbdc05f307
eeintech <eeintech@eeinte.ch>
2020-06-01 18:19:55 -0500

    Updated form text widget, clean-up view and task and started to add comments to code

--- download/views.py
+++ download/views.py
@@ -1,39 +1,33 @@
 from django.shortcuts import render
-from django.views.decorators.csrf import csrf_exempt
-from django.views.decorators.cache import never_cache
 
 from .forms import DownloadForm
-from .tasks import ProcessDownload#, CheckDownloadProgress
+from .tasks import ProcessDownload
 
 def demo_view(request):
+	# If method is POST, process form data and start task
 	if request.method == 'POST':
+		# Get form instance
 		form = DownloadForm(request.POST)
 
 		if form.is_valid():
+			# Retrieve URL from form data
 			url = form.cleaned_data['url']
+
 			print(f'Downloading: {url}')
 			# Create Task
 			download_task = ProcessDownload.delay(url)
+			# Get ID
 			task_id = download_task.task_id
+			# Print Task ID
 			print (f'Celery Task ID: {task_id}')
 
+			# Return demo view with Task ID
 			return render(request, 'progress.html', {'form': form, 'task_id': task_id})
+		else:
+			# Return demo view
+			return render(request, 'progress.html', {'form': form})
 	else:
+		# Get form instance
 		form = DownloadForm()
-
+		# Return demo view
 		return render(request, 'progress.html', {'form': form})
-
-@csrf_exempt
-@never_cache
-def success_view(request, task_id):
-	print(f'Success View for [{task_id}]')
-	if request.method == 'POST':
-		response_json = request.POST
-		response_json = json.dumps(response_json)
-		data = json.loads(response_json)
-		payload = json.loads(data['data'])
-
-	if data:
-		print(f'Processing success data: {data}')
-
-	return render(request, 'progress.html', {'task_id': task_id})
